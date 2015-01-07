815d1c5b34fc32ba5dba24768d8fae644e8a7cad
xiewulong <xiewulong@vip.qq.com>
2015-01-08 02:05:37 +0800

    migrations

---
+++ migrations/m150107_174237_wechat.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+
+class m150107_174237_wechat extends \yii\db\Migration{
+
+	public function up(){
+		$tableOptions = 'engine=innodb character set utf8';
+		if($this->db->driverName === 'mysql') {
+			$tableOptions .= ' collate utf8_unicode_ci';
+		}
+
+		$this->createTable('{{%wechat}}', [
+			'appid' => Schema::TYPE_STRING . '(50) primary key comment "AppID(应用ID)"',
+			'appsecret' => Schema::TYPE_STRING . '(50) not null comment "AppSecret(应用密钥)"',
+			'token' => Schema::TYPE_STRING . '(50) not null comment "Token(令牌)"',
+			'aeskey' => Schema::TYPE_STRING . '(50) comment "EncodingAESKey(消息加解密密钥)"',
+			'mode' => Schema::TYPE_BOOLEAN . ' not null default 1 comment "消息加解密模式: 0明文模式, 1兼容模式, 2安全模式"',
+			'access_token' => Schema::TYPE_TEXT. ' comment "access_token"',
+			'expires_in' => Schema::TYPE_INTEGER . ' not null default 0 comment "access_token有效时长(秒)"',
+			'created_at' => Schema::TYPE_INTEGER . ' not null comment "创建时间"',
+			'updated_at' => Schema::TYPE_INTEGER . ' not null comment "更新时间"',
+		], $tableOptions . ' comment="微信app"');
+	}
+
+	public function down(){
+		$this->dropTable('{{%wechat}}');
+	}
+
+}
--- migrations/schema-mysql.sql
+++ migrations/schema-mysql.sql
@@ -1,18 +1,17 @@
 CREATE TABLE `tbpre_wechat` (
-	`appid` VARCHAR(50) NOT NULL COMMENT 'AppID(应用ID)',
-	`appsecret` VARCHAR(50) NOT NULL COMMENT 'AppSecret(应用密钥)',
-	`token` VARCHAR(50) NOT NULL COMMENT 'Token(令牌)',
-	`aeskey` VARCHAR(50) NULL DEFAULT NULL COMMENT 'EncodingAESKey(消息加解密密钥)',
-	`mode` TINYINT(1) UNSIGNED NOT NULL DEFAULT '1' COMMENT '消息加解密模式: 0明文模式, 1兼容模式, 2安全模式',
-	`access_token` TEXT NULL COMMENT 'access_token',
-	`expires_in` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'access_token有效时长(秒)',
-	`created_at` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0' COMMENT '创建时间',
-	`updated_at` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0' COMMENT '更新时间',
-	PRIMARY KEY (`appid`),
-	UNIQUE INDEX `appid` (`appid`)
+	`appid` VARCHAR(50) NOT NULL COMMENT 'AppID(应用ID)' COLLATE 'utf8_unicode_ci',
+	`appsecret` VARCHAR(50) NOT NULL COMMENT 'AppSecret(应用密钥)' COLLATE 'utf8_unicode_ci',
+	`token` VARCHAR(50) NOT NULL COMMENT 'Token(令牌)' COLLATE 'utf8_unicode_ci',
+	`aeskey` VARCHAR(50) NULL DEFAULT NULL COMMENT 'EncodingAESKey(消息加解密密钥)' COLLATE 'utf8_unicode_ci',
+	`mode` TINYINT(1) NOT NULL DEFAULT '1' COMMENT '消息加解密模式: 0明文模式, 1兼容模式, 2安全模式',
+	`access_token` TEXT NULL COMMENT 'access_token' COLLATE 'utf8_unicode_ci',
+	`expires_in` INT(11) NOT NULL DEFAULT '0' COMMENT 'access_token有效时长(秒)',
+	`created_at` INT(11) NOT NULL COMMENT '创建时间',
+	`updated_at` INT(11) NOT NULL COMMENT '更新时间',
+	PRIMARY KEY (`appid`)
 )
 COMMENT='微信app'
-COLLATE='utf8_general_ci'
+COLLATE='utf8_unicode_ci'
 ENGINE=InnoDB;
 
 INSERT INTO `tbpre_wechat` (`appid`, `appsecret`, `token`, `aeskey`, `mode`, `created_at`, `updated_at`) VALUES
