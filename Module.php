45679e261740d627cda910ff8cfb28a96862ae99
xiewulong <xiewulong@vip.qq.com>
2015-01-10 13:22:30 +0800

    use manager

--- Module.php
+++ Module.php
@@ -8,6 +8,18 @@ use yii\wechat\models\Wechat;
 
 class Module extends \yii\base\Module{
 
+	public $defaultRoute = 'api';
+
+	public $defaultComponent = 'wechat';
+
+	public $manager;
+
+	public function init(){
+		parent::init();
+
+		$this->manager = Yii::createObject(Yii::$app->components[$this->defaultComponent]);
+	}
+
 	public function checkSignature($appid){
 		if($wechat = Wechat::findOne($appid)){
 			$token = $wechat->token;
--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -5,9 +5,11 @@ namespace yii\wechat\controllers;
 use Yii;
 use yii\web\Controller;
 
-class DefaultController extends Controller{
+class ApiController extends Controller{
+
+	public $defaultAction = 'public';
 	
-	public function actionIndex($appid){
+	public function actionPublic($appid){
 		if($echostr = Yii::$app->request->get('echostr')){
 			return $this->module->checkSignature($appid) ? $echostr : false;
 		}
