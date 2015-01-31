46c552b65d27a0e0eca070ce8f3759ef9b15f5d8
xiewulong <xiewulong@vip.qq.com>
2015-01-31 14:21:26 +0800

    wechat

--- migrations/m150106_010000_wechat.php
+++ migrations/m150106_010000_wechat.php
@@ -2,7 +2,7 @@
 
 use yii\db\Schema;
 
-class m150107_174237_wechat extends \yii\db\Migration{
+class m150106_010000_wechat extends \yii\db\Migration{
 
 	public function up(){
 		$tableOptions = 'engine=innodb character set utf8';
