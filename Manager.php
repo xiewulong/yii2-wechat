ad87b837f0713ff7a48d125a45166e81be6d29be
xiewulong <xiewulong@vip.qq.com>
2016-02-03 17:04:25 +0800

    wechat

--- Manager.php
+++ Manager.php
@@ -5,15 +5,17 @@
  * https://github.com/xiewulong/yii2-wechat
  * https://raw.githubusercontent.com/xiewulong/yii2-wechat/master/LICENSE
  * create: 2014/12/30
- * update: 2016/2/2
+ * update: 2016/2/3
  * version: 0.0.1
  */
 
 namespace yii\wechat;
 
 use yii\base\ErrorException;
+use yii\data\Pagination;
 use yii\helpers\Json;
 use yii\wechat\models\Wechat;
+use yii\wechat\models\WechatUser;
 
 class Manager {
 
@@ -48,6 +50,170 @@ class Manager {
 	}
 
 	/**
+	 * 设置用户备注名
+	 * @method updateUserRemark
+	 * @since 0.0.1
+	 * @param {int} $id 用户id
+	 * @return {none}
+	 * @example \Yii::$app->wechat->updateUserRemark($id);
+	 */
+	public function updateUserRemark($id) {
+		$user = WechatUser::findOne($id);
+		if(!$user){
+			throw new ErrorException('数据查询失败');
+		}
+
+		$data = $this->getData('/user/info/updateremark', [
+			'access_token' => $this->getAccessToken(),
+		], Json::encode([
+			'openid' => $user->openid,
+			'remark' => $user->remark,
+		]));
+
+		return $this->errcode == 0;
+	}
+
+	/**
+	 * 刷新用户基本信息
+	 * @method refreshUser
+	 * @since 0.0.1
+	 * @param {int} $id 用户id
+	 * @return {none}
+	 * @example \Yii::$app->wechat->refreshUser($id);
+	 */
+	public function refreshUser($id) {
+		$user = WechatUser::findOne($id);
+		if(!$user){
+			throw new ErrorException('数据查询失败');
+		}
+
+		$data = $this->getData('/user/info', [
+			'access_token' => $this->getAccessToken(),
+			'openid' => $user->openid,
+			'lang' => \Yii::$app->language,
+		]);
+
+		$user->subscribe = $_user['subscribe'];
+		if($user->subscribe == 1){
+			$user->subscribe_time = $_user['subscribe_time'];
+			$user->nickname = $_user['nickname'];
+			$user->sex = $_user['sex'];
+			$user->country = $_user['country'];
+			$user->city = $_user['city'];
+			$user->province = $_user['province'];
+			$user->language = $_user['language'];
+			$user->headimgurl = $_user['headimgurl'];
+			$user->remark = $_user['remark'];
+			$user->groupid = $_user['groupid'];
+		}
+		if(isset($_user['unionid'])) {
+			$user->unionid = $_user['unionid'];
+		}
+		$user->save();
+	}
+
+	/**
+	 * 刷新所有用户基本信息
+	 * @method refreshUsers
+	 * @since 0.0.1
+	 * @param {int} [$page=1] 页码
+	 * @return {none}
+	 * @example \Yii::$app->wechat->refreshUsers($page);
+	 */
+	public function refreshUsers($page = 1) {
+		$query = WechatUser::find()->where(['appid' => $this->wechat->appid])->select('openid');
+
+		$pagination = new Pagination([
+			'totalCount' => $query->count(),
+			'defaultPageSize' => 100,
+			'pageSizeLimit' => [0, 100],
+		]);
+		$pagination->setPage($page - 1, true);
+
+		$users = $query->offset($pagination->offset)
+			->limit($pagination->limit)
+			->asArray()
+			->all();
+
+		$user_list = [];
+		foreach($users as $user){
+			$user['lang'] = \Yii::$app->language;
+			$user_list['user_list'][] = $user;
+		}
+
+		if($user_list){
+			$data = $this->getData('/user/info/batchget', [
+				'access_token' => $this->getAccessToken(),
+			], Json::encode($user_list));
+			foreach($data['user_info_list'] as $_user){
+				$user = WechatUser::findOne(['appid' => $this->wechat->appid, 'openid' => $_user['openid']]);
+				if(!$user){
+					$user = new WechatUser;
+					$user->appid = $this->wechat->appid;
+					$user->openid = $_user['openid'];
+				}
+				$user->subscribe = $_user['subscribe'];
+				if($user->subscribe == 1){
+					$user->subscribe_time = $_user['subscribe_time'];
+					$user->nickname = $_user['nickname'];
+					$user->sex = $_user['sex'];
+					$user->country = $_user['country'];
+					$user->city = $_user['city'];
+					$user->province = $_user['province'];
+					$user->language = $_user['language'];
+					$user->headimgurl = $_user['headimgurl'];
+					$user->remark = $_user['remark'];
+					$user->groupid = $_user['groupid'];
+				}
+				if(isset($_user['unionid'])) {
+					$user->unionid = $_user['unionid'];
+				}
+				$user->save();
+			}
+		}
+
+		if($page < $pagination->pageCount){
+			$this->refreshUsers($page + 1);
+		}
+	}
+
+	/**
+	 * 获取用户列表
+	 * @method getUsers
+	 * @since 0.0.1
+	 * @param {string} [$next_openid] 第一个拉取的OPENID, 不填默认从头开始拉取
+	 * @return {none}
+	 * @example \Yii::$app->wechat->getUsers($next_openid);
+	 */
+	public function getUsers($next_openid = null) {
+		$data = $this->getData('/user/get', [
+			'access_token' => $this->getAccessToken(),
+			'next_openid' => $next_openid,
+		]);
+
+		if($data['count'] && isset($data['data']) && isset($data['data']['openid'])) {
+			foreach($data['data']['openid'] as $openid) {
+				if($user = WechatUser::findOne(['appid' => $this->wechat->appid, 'openid' => $openid])) {
+					if($user->subscribe == 0) {
+						$user->subscribe = 1;
+						$user->save();
+					}
+				} else {
+					$user = new WechatUser;
+					$user->appid = $this->wechat->appid;
+					$user->openid = $openid;
+					$user->subscribe = 1;
+					$user->save();
+				}
+			}
+		}
+
+		if($data['next_openid']) {
+			$this->getUsers($data['next_openid']);
+		}
+	}
+
+	/**
 	 * 获取微信服务器IP地址
 	 * @method getCallbackIp
 	 * @since 0.0.1
@@ -73,6 +239,7 @@ class Manager {
 		$data = $this->getData('/getcallbackip', [
 			'access_token' => $this->getAccessToken(),
 		]);
+
 		if(isset($data['ip_list'])) {
 			$this->wechat->ip_list = Json::encode($data['ip_list']);
 			return $this->wechat->save();
@@ -85,7 +252,7 @@ class Manager {
 	 * 获取接口调用凭据
 	 * @method getAccessToken
 	 * @since 0.0.1
-	 * @return {boolean}
+	 * @return {string}
 	 * @example \Yii::$app->wechat->getAccessToken();
 	 */
 	public function getAccessToken() {
@@ -121,7 +288,7 @@ class Manager {
 	 * 生成随机字符串
 	 * @method generateRandomString
 	 * @since 0.0.1
-	 * @param {int} $len 长度
+	 * @param {int} [$len=32] 长度
 	 * @return {string}
 	 * @example \Yii::$app->wechat->generateRandomString($len);
 	 */
@@ -163,6 +330,18 @@ class Manager {
 	}
 
 	/**
+	 * 获取接口完整访问地址
+	 * @method getApiUrl
+	 * @since 0.0.1
+	 * @param {string} $action 接口名称
+	 * @param {array} $query 参数
+	 * @return {string}
+	 */
+	private function getApiUrl($action, $query) {
+		return $this->api . $action . (empty($query) ? '' : '?' . http_build_query($query));
+	}
+
+	/**
 	 * 获取信息
 	 * @method getMessage
 	 * @since 0.0.1
@@ -175,18 +354,6 @@ class Manager {
 		}
 
 		return isset($this->messages[$status]) ? $this->messages[$status] : null;
-	}
-
-	/**
-	 * 获取接口完整访问地址
-	 * @method getApiUrl
-	 * @since 0.0.1
-	 * @param {string} $action 接口名称
-	 * @param {array} $query 参数
-	 * @return {string}
-	 */
-	private function getApiUrl($action, $query) {
-		return $this->api . $action . (empty($query) ? '' : '?' . http_build_query($query));
 	}
 
 	/**
@@ -203,15 +370,19 @@ class Manager {
 		curl_setopt($curl, CURLOPT_URL, $url);
 		curl_setopt($curl, CURLOPT_HEADER, 0);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
 		if(!empty($data)) {
 			curl_setopt($curl, CURLOPT_POST, 1);
 			curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 		}
+
 		if(!empty($useragent)) {
 			curl_setopt($curl, CURLOPT_USERAGENT, $useragent);
 		}
+
 		$data = curl_exec($curl);
 		curl_close($curl);
+
 		return $data;
 	}
 
--- migrations/m160115_081338_wechat_user.php
+++ migrations/m160115_081338_wechat_user.php
@@ -12,9 +12,21 @@ class m160115_081338_wechat_user extends \yii\db\Migration {
 
 		$this->createTable('{{%wechat_user}}', [
 			'id' => Schema::TYPE_PK . ' comment "id"',
+			'userid' => Schema::TYPE_INTEGER . ' not null comment "用户id"',
 			'appid' => Schema::TYPE_STRING . '(50) not null comment "应用id"',
 			'openid' => Schema::TYPE_STRING . ' not null comment "绑定微信用户id, 对应appid唯一"',
-			'userid' => Schema::TYPE_INTEGER . ' not null comment "用户id"',
+			'unionid' => Schema::TYPE_STRING . ' comment "UnionID"',
+			'subscribe' => Schema::TYPE_BOOLEAN . ' not null default 0 comment "订阅状态: 0未订阅, 1已订阅"',
+			'subscribe_time' => Schema::TYPE_INTEGER . ' not null default 0 comment "关注时间"',
+			'nickname' => Schema::TYPE_STRING . '(50) comment "昵称"',
+			'remark' => Schema::TYPE_STRING . '(50) comment "备注名, 长度必须小于30字符"',
+			'groupid' => Schema::TYPE_INTEGER . ' not null default 0 comment "所在分组ID"',
+			'sex' => Schema::TYPE_BOOLEAN . ' not null default 0 comment "性别: 0未知, 1男性, 2女性"',
+			'country' => Schema::TYPE_STRING . '(50) comment "所在国家"',
+			'city' => Schema::TYPE_STRING . '(50) comment "所在城市"',
+			'province' => Schema::TYPE_STRING . '(50) comment "所在省份"',
+			'language' => Schema::TYPE_STRING . '(50) comment "语言"',
+			'headimgurl' => Schema::TYPE_TEXT . ' comment "头像, 尺寸: 0(640*640), 46, 64, 96, 132"',
 			'created_at' => Schema::TYPE_INTEGER . ' not null comment "创建时间"',
 			'updated_at' => Schema::TYPE_INTEGER . ' not null comment "更新时间"',
 		], $tableOptions . ' comment="公众号用户绑定"');
